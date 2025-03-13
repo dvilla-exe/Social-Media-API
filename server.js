0166ba1a94067c1d597edbccbaba71af8ed5d5f3
Daniel V <d.villa.exe@gmail.com>
2025-03-13 15:09:59 -0400

    edit mongoose connection

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const thoughtRoutes = require('./routes/thoughtRoutes');
 const reactionRoutes = require('./routes/reactionRoutes');
 require('./config/connection');
 
+mongoose.connect('mongodb://localhost/social_network_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
